cea557a803302deb99b78116a068a42d549f93bc
Sparsh Garg <sparsh.garg2019@outlook.com>
2020-07-24 05:06:18 +0530

    Carousel fixed

--- js/main.js
+++ js/main.js
@@ -315,29 +315,6 @@
 	};
 
 	
-	$('#recipeCarousel').carousel({
-		interval: 10000
-	  })
-	  
-	  $('.carousel .carousel-item').each(function(){
-		  var minPerSlide = 3;
-		  var next = $(this).next();
-		  if (!next.length) {
-		  next = $(this).siblings(':first');
-		  }
-		  next.children(':first-child').clone().appendTo($(this));
-		  
-		  for (var i=0;i<minPerSlide;i++) {
-			  next=next.next();
-			  if (!next.length) {
-				  next = $(this).siblings(':first');
-				}
-			  
-			  next.children(':first-child').clone().appendTo($(this));
-			}
-	  });
-	  
-	  
 
 
 	
@@ -353,6 +330,7 @@
 		dropdown();
 		goToTop();
 		loaderPage();
+		carousels();
 	});
 
 
